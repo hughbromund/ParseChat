452f8708140335765f9ca45f2702c8fa7a75459f
Hugh Bromund <hughbromund44@gmail.com>
2020-02-19 18:09:03 -0500

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Hugh Bromund on 2/19/20.
+//  Copyright © 2020 Hugh Bromund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
